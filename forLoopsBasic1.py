9da82dd362c6091fed4fb4a434ce18a3f7931149
Dave Naugler <davenaugler@gmail.com>
2019-09-02 19:31:46 -0600

    Update forLoopsBasic1.py

--- forLoopsBasic1.py
+++ forLoopsBasic1.py
@@ -1,18 +1,15 @@
 #1 Basic - Print all integers from 0 to 150.
-
 for x in range(151):
     print(x)
 
 
 #2 Multiples of Five - Print all the multiples of 5 from 5 to 1,000
-
 for x in range (5, 1001, 5):
     print(x)
 
 
 #3 Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, 
 # print "Coding" instead. If divisible by 10, print "Coding Dojo".
-
 for x in range(1,101):
     if x % 10 == 0:
         print('Coding Dojo')
@@ -23,7 +20,6 @@ for x in range(1,101):
 
 
 #4 Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
-
 sum = 0
 for x in range(500000):
     if x % 2 != 0:
@@ -39,7 +35,6 @@ for x in range(2018, 0, -4):
 #6 Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and 
 # going through highNum, print only the integers that are a multiple of mult. 
 # For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
-
 lowNum = 2
 highNum = 9
 mult = 3
